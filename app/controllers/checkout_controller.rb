281f7455121978d2b93a6fa5bc74157488a58444
ColtonCGraham <coltoncgraham@hotmail.com>
2023-08-25 14:57:41 -0500

    Search form and nav bar layouts

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
 class CheckoutController < ApplicationController
-  def new;end
+  def new
+    if session[:accountId]
+      @account = Account.find_by(id: session[:accountId])
+      else
+      @account = nil
+      redirect_to sign_in_path, alert: 'Log in before checkout.'
+    end
+
+  end
+
 
   def create
     puts params
+    @account = Account.find_by(id: session[:accountId])
+    puts @account.name
   end
 end
